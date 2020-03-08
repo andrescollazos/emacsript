f92ed894b3b99106714d2f45628db4b61883403a
andrescollazos <afcollazosr@gmail.com>
2020-03-08 17:24:54 -0500

    ES7: Includes y elevar a una potencia

---
+++ src/es7/index.js
@@ -0,0 +1,20 @@
+// --------------------------------------------------------------------------------
+// INCLUDES
+// Verificar si hay un elemento determinado en un arreglo o string
+
+let numbers = [1, 2, 3, 4, 5, 6]
+if (numbers.includes(7)) {
+    console.log('Sí se encuentra el valor 7')
+} else {
+    console.log('No se encuentra el valor 7')
+}
+
+// --------------------------------------------------------------------------------
+// ELEVAR A UNA POTENCIA
+// Al utilizar el doble asterisco (**)
+
+let base = 2
+let exponent = 3
+let result = base ** exponent
+console.log(result)
+
